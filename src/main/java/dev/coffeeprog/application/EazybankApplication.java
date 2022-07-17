97fe74c84fd0b762ca6e46e2798b15fecb4c5f13
Chad Botting <coffeeprogrammer0@gmail.com>
2022-07-17 14:51:49 -0400

    some change towards DB

--- src/main/java/dev/coffeeprog/application/EazybankApplication.java
+++ src/main/java/dev/coffeeprog/application/EazybankApplication.java
@@ -1,4 +1,4 @@
-package dev.coffeeprog.eazybank;
+package dev.coffeeprog.application;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -8,7 +8,9 @@ import org.springframework.context.annotation.ComponentScans;
 @SpringBootApplication
 @ComponentScans({
 		@ComponentScan("dev.coffeeprog.controller"),
-		@ComponentScan("dev.coffeeprog.config")
+		@ComponentScan("dev.coffeeprog.config"),
+		@ComponentScan("dev.coffeeprog.repository"),
+		@ComponentScan("dev.coffeeprog.model")
 })
 public class EazybankApplication {
 
--- src/main/java/dev/coffeeprog/config/EasyBankUserDetails.java
+++ src/main/java/dev/coffeeprog/config/EasyBankUserDetails.java
@@ -1,8 +1,28 @@
 package dev.coffeeprog.config;
 
-
+import dev.coffeeprog.model.Customer;
+import dev.coffeeprog.model.SecurityCustomer;
+import dev.coffeeprog.repository.CustomerRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
 
-//@Service
-public class EasyBankUserDetails {
+import java.util.List;
+
+@Service
+public class EasyBankUserDetails implements UserDetailsService {
+
+    @Autowired
+    private CustomerRepository customerRepository;
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        List<Customer> customer = customerRepository.findByEmail(username);
+        if (customer.size() == 0) {
+            throw new UsernameNotFoundException("User details not found for the user : " + username);
+        }
+        return new SecurityCustomer(customer.get(0));
+    }
 }
